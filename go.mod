b1c98f146fa54bdde07cf7905c0cddde91dbd566
M <max.zhu.sss@gmail.com>
2021-09-08 21:58:10 -0400

    upload single file

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module shopifybackend
 
 go 1.16
+
+require (
+	github.com/aws/aws-sdk-go v1.40.39 // indirect
+	golang.org/x/net v0.0.0-20210908191846-a5e095526f91 // indirect
+	golang.org/x/text v0.3.7 // indirect
+    github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0 
+)
--- main.go
+++ main.go
@@ -1,7 +1,120 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+type Upload struct {
+	Filename string `json:"filename"`
+	Path     string `json:"path"`
+	Author   string `json:"author"`
+}
+
+var accesskey string      // access key
+var secretkey string      // secret key
+var awsRegion string      // region
+var bucket string         // bucketname
+var sess *session.Session // session created for s3 connection
+
+func uploadPicture(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+
+	decoder := json.NewDecoder(r.Body) // read body of request
+	var uploaded Upload
+
+	log.Printf("Unable to decode")
+	err := decoder.Decode(&uploaded)
+
+	log.Printf(r.RequestURI)
+	// data, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		log.Printf("Unable to decode")
+	}
+	uploader := s3manager.NewUploader(sess)
+
+	log.Printf("uploaded.path: " + uploaded.Path)
+
+	file, err := os.Open("./Birds.jpg")
+
+	if err != nil {
+		log.Printf("Unable to open file %q", err)
+	}
+
+	defer file.Close()
+
+	fin, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+		ACL:    aws.String("public-read"),
+		Key:    aws.String(uploaded.Filename),
+		Body:   file,
+	})
+
+	log.Printf(fin.UploadID)
+
+}
 
 func main() {
 	fmt.Println("Hello, world.")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/upload", uploadPicture).Methods("POST")
+
+	accesskey = os.Getenv("AWS_ACCESS_KEY")
+	secretkey = os.Getenv("AWS_SECRET_ACCESS_KEY")
+	awsRegion = os.Getenv("AWS_REGION")
+	bucket = os.Getenv("BUCKET")
+
+	sess, err = session.NewSession(
+		&aws.Config{
+			Region: aws.String(awsRegion),
+			Credentials: credentials.NewStaticCredentials(
+				accesskey,
+				secretkey,
+				"", // a token will be created when the session it's used.
+			),
+		})
+	if err != nil {
+		panic(err)
+	}
+
+	svc := s3.New(sess)
+
+	result, err := svc.ListBuckets(nil)
+	if err != nil {
+		log.Printf("Unable to list buckets, %v", err)
+	}
+
+	fmt.Println("Buckets:")
+
+	for _, b := range result.Buckets {
+		fmt.Printf("* %s created on %s\n",
+			aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
+	}
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+
 }
