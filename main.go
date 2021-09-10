05209ff9bf94bd1a2234fe6d671c5ef7360d9694
M <max.zhu.sss@gmail.com>
2021-09-09 23:21:42 -0400

    zip test and photos

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"archive/zip"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
@@ -17,7 +19,14 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
-type Upload struct {
+type UploadSingle struct {
+	Filename string `json:"filename"`
+	Filetype string `json:"filetype"`
+	Path     string `json:"path"`
+	Author   string `json:"author"`
+}
+
+type UploadZip struct {
 	Filename string `json:"filename"`
 	Path     string `json:"path"`
 	Author   string `json:"author"`
@@ -29,7 +38,7 @@ var awsRegion string      // region
 var bucket string         // bucketname
 var sess *session.Session // session created for s3 connection
 
-func uploadPicture(w http.ResponseWriter, r *http.Request) {
+func bulkUpload(w http.ResponseWriter, r *http.Request) { // upload a zip containing files
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
@@ -37,13 +46,11 @@ func uploadPicture(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "POST")
 
 	decoder := json.NewDecoder(r.Body) // read body of request
-	var uploaded Upload
+	var uploaded UploadZip
 
-	log.Printf("Unable to decode")
 	err := decoder.Decode(&uploaded)
 
 	log.Printf(r.RequestURI)
-	// data, err := ioutil.ReadAll(r.Body)
 
 	if err != nil {
 		log.Printf("Unable to decode")
@@ -52,27 +59,108 @@ func uploadPicture(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("uploaded.path: " + uploaded.Path)
 
-	file, err := os.Open("./Birds.jpg")
+	archive, err := zip.OpenReader(uploaded.Path)
 
 	if err != nil {
+		log.Fatalf("Can't Unzip")
+	}
+
+	defer archive.Close()
+
+	// unzip and iterate through
+	for _, f := range archive.File {
+		// filePath := filepath.Join(dst, f.Name)
+		log.Printf("unzipping file " + f.Name)
+
+		fileType := filepath.Ext(f.Name)
+
+		if fileType != ".jpg" && fileType != ".jpeg" && fileType != ".png" {
+			log.Printf("unrecognized file, skipping ")
+			continue
+		}
+
+		if f.FileInfo().IsDir() {
+			fmt.Println("found directory")
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		file, err := os.Open(f.Name)
+
+		if err != nil {
+			log.Printf("Unable to open file %q", err)
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		meta := make(map[string]*string)
+
+		meta["author"] = aws.String(uploaded.Author)
+
+		fin, err := uploader.Upload(&s3manager.UploadInput{
+			Bucket:   aws.String(bucket),
+			ACL:      aws.String("public-read"),
+			Key:      aws.String(file.Name()), // picture is prefixed by author name
+			Body:     file,
+			Metadata: meta,
+		})
+
+		log.Printf(fin.UploadID)
+
+		file.Close()
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+}
+
+func uploadPicture(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+
+	decoder := json.NewDecoder(r.Body) // read body of request
+	var uploaded UploadSingle
+
+	err := decoder.Decode(&uploaded)
+
+	if err != nil {
+		log.Printf("Unable to decode")
+	}
+	uploader := s3manager.NewUploader(sess)
+
+	log.Printf("uploaded.path: " + uploaded.Path)
+
+	file, err := os.Open(uploaded.Path)
+
+	if err != nil {
+		w.WriteHeader(http.StatusForbidden)
 		log.Printf("Unable to open file %q", err)
 	}
 
 	defer file.Close()
 
+	meta := make(map[string]*string)
+
+	meta["author"] = aws.String(uploaded.Author)
+
 	fin, err := uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(bucket),
-		ACL:    aws.String("public-read"),
-		Key:    aws.String(uploaded.Filename),
-		Body:   file,
+		Bucket:   aws.String(bucket),
+		ACL:      aws.String("public-read"),
+		Key:      aws.String(uploaded.Filename + "." + uploaded.Filetype), // picture is prefixed by author name
+		Body:     file,
+		Metadata: meta,
 	})
 
 	log.Printf(fin.UploadID)
 
+	w.Write([]byte("Picture successfully uploaded!\n"))
+
 }
 
 func main() {
-	fmt.Println("Hello, world.")
 
 	err := godotenv.Load()
 	if err != nil {
@@ -82,6 +170,7 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/upload", uploadPicture).Methods("POST")
+	router.HandleFunc("/api/zipupload", bulkUpload).Methods("POST")
 
 	accesskey = os.Getenv("AWS_ACCESS_KEY")
 	secretkey = os.Getenv("AWS_SECRET_ACCESS_KEY")
@@ -114,6 +203,19 @@ func main() {
 		fmt.Printf("* %s created on %s\n",
 			aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
 	}
+
+	// resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(bucket)})
+	// if err != nil {
+	// 	log.Printf("Unable to list items in bucket %q, %v", bucket, err)
+	// }
+
+	// for _, item := range resp.Contents {
+	// 	fmt.Println("Name:         ", *item.Key)
+	// 	fmt.Println("Last modified:", *item.LastModified)
+	// 	fmt.Println("Size:         ", *item.Size)
+	// 	fmt.Println("Storage class:", *item.StorageClass)
+	// 	fmt.Println("")
+	// }
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 
